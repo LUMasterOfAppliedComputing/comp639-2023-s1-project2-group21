61e3905bb1de15654d9566e2b0c8e46b067196f5
AlexShao <Alex.Shao@lincolnuni.ac.nz>
2023-05-09 22:35:17 +1200

    Update connect.py

--- connect.py
+++ connect.py
@@ -2,7 +2,7 @@
 # Usually called 'Local Instance'
 dbuser = "root" #PUT YOUR MySQL username here - usually root
 
-dbpass = "123456" #PUT YOUR PASSWORD HERE
+dbpass = "12345" #PUT YOUR PASSWORD HERE
 dbhost = "localhost"
 dbport = "3306"
 dbname = "639project_2"
